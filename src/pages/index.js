352dacf5a94dfb71eb4cd8b7d3d9e2f22cf59c94
Hendrik Schäfer <hendrikschafer@Hendriks-MBP.local>
2019-12-05 08:12:25 +0100

    home new

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout";
 import Img from "gatsby-image";
 import { SinglePost, PostInfo, PreTitle, Title, Subtitle, Update, Freshnew, Excrept, Card, CardColumns }  from "../components/style/emo-home-posts";
 import Bild from "../img/post/blog-1.png";
-import RechercheBild from "../img/post/Sachrecherche-Bundesarchiv-Berlin.jpg";
+import RechercheBild from "../img/post/R-9361-IX-KARTEI-9621548.jpg";
 
 let minutes = 1000 * 60;
 let hours = minutes * 60;
@@ -15,24 +15,26 @@ export default ({ data }) => {
   return (
     <Layout>
     <>
-        <div><h1 className="customHeadline">Thomas Pruschwitz</h1></div>
-        <div style={{display: 'flex'}}>
 
-          <div style={{width:'48%', padding:'40px', marginRight:'20px', backgroundColor:'#f9f9f9', border:'1px solid #4d4d4d20', borderRadius:'6px'}}><img style={{width:'100%'}} src={Bild} />
+        <div style={{display: 'flex', maxWidth:'100%'}}>
+
+          <div class="left" style={{width:'48%', padding:'40px',marginRight:'20px', backgroundColor:'#f9f9f9', border:'1px solid #4d4d4d20', borderRadius:'6px', textAlign:'center'}}><img style={{width:'90%'}} src={RechercheBild} />
           <h2 style={{marginLeft: 'auto',
     marginRight: 'auto',
-    padding: '15px', width:'100%'}}>Ein Brief Hitlers mit mehreren Unbekannten</h2>
-    <div style={{lineHeight: '1.3', fontWeight:'400', width:'100%', marginBottom:'30px', textAlign:'center'}}>Vor etwa 90 Jahren, am 8. Dezember 1929, wählten die Thüringerinnen und Thüringer ihren fünften demokratischen Landtag der Weimarer ...</div>
-            <div style={{textAlign:' center', marginLeft: 'auto', marginRight: 'auto'}}><a href="/brief-hitler"><button class="button-x" value="test">Zum Artikel</button></a ></div>
+    padding: '15px', width:'100%'}}>Ad Acta Recherchen</h2>
+    <div style={{lineHeight: '1.3', fontWeight:'400', width:'100%', marginBottom:'30px', textAlign:'center'}}>Kostenfreie Anfragen zu Recherchen im Bundesarchiv Berlin</div>
+            <div style={{textAlign:' center', marginLeft: 'auto', marginRight: 'auto'}}><a target="_blank" href="https://www.recherche-bundesarchiv.de/"><button class="button-x" value="test">Hier</button></a ></div>
           </div>
-          <div style={{width:'48%', padding:'40px',backgroundColor:'#f9f9f9', border:'1px solid #4d4d4d20', borderRadius:'6px', textAlign:'center'}}><img style={{width:'90%'}} src={RechercheBild} />
-          <h2 style={{marginLeft: 'auto',
-    marginRight: 'auto',
-    padding: '15px', width:'100%'}}>Recherchedienst Bundesarchiv</h2>
-    <div style={{lineHeight: '1.3', fontWeight:'400', width:'100%', marginBottom:'30px', textAlign:'center'}}>Rechercheaufträge für Insititutionen und Privatpersonen im Bundesarchiv Berlin</div>
-            <div style={{textAlign:' center', marginLeft: 'auto', marginRight: 'auto'}}><a target="_blank" href="https://www.recherche-bundesarchiv.de/"><button class="button-x" value="test">Zu Ad Acta</button></a ></div>
-          </div>
+
+
+        <div class="right" style={{width:'48%', padding:'40px', backgroundColor:'#f9f9f9', border:'1px solid #4d4d4d20', borderRadius:'6px'}}><img style={{width:'100%'}} src={Bild} />
+        <h2 style={{marginLeft: 'auto',
+  marginRight: 'auto',
+  padding: '15px', width:'100%'}}>Tatort Zeitgeschichte</h2>
+  <div style={{lineHeight: '1.3', fontWeight:'400', width:'100%', marginBottom:'30px', textAlign:'center'}}>Ein Brief Hitlers mit mehreren <br/>Unbekannten</div>
+          <div style={{textAlign:' center', marginLeft: 'auto', marginRight: 'auto'}}><a href="/tatort-zeitgeschichte"><button class="button-x">Mehr</button></a ></div>
         </div>
+            </div>
     </>
     </Layout>
   );
